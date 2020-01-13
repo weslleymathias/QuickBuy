027dfa108785e5fdefec90b18a1813d8223ed2c6
Weslley Mathias <weslley.mathias@mereo.com>
2020-01-13 11:56:31 -0300

    Finalizando a seção 6.

--- QuickBuy.Web/ClientApp/src/app/app.module.ts
+++ QuickBuy.Web/ClientApp/src/app/app.module.ts
@@ -10,13 +10,16 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+import { ProdutoComponent } from './produto/produto.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +29,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'produto', component: ProdutoComponent }
     ])
   ],
   providers: [],
---
+++ QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "app-produto",
+  template: "<html><body>{{ obterNome() }}</body></html>"
+})
+export class ProdutoComponent {// Nome das classes começando com maiúsculo por conta da convenção PascalCase
+
+  /* camelCase para variáveis, atributos e nomes das funções */
+  public nome: string;
+  public liberadoParaVenda: boolean;
+
+  public obterNome(): string {
+    return "Samsung";
+  }
+}
